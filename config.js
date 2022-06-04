c758e12ee20e5003837c95d6d621bb97fd4bf1fc
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-06-05 01:19:37 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,54 +1,126 @@
-const fs = require('fs')
-const chalk = require('chalk')
+//=============『 Utama 』================== //
+global.owner = ['62895627514070'] 
+global.mods = [] 
+global.prems = [] 
 
-// Website Api
-global.APIs = {
-	zenz: 'https://zenzapi.xyz',
+//=============『 Info Owner 』============== //
+global.nameowner = '.'
+global.numberowner = '6281395981439'
+global.instagram = 'https://instagram.com/hyzer_stfu'
+global.github = 'https://github.com/Hyzerr'
+global.dana = '087892711054'
+global.pulsa = '085212023766'
+global.gopay = '087892711054'
+
+//=============『 Info Bot 』=================//
+global.namebot = 'ShinomiyaBot-Md'
+global.gc = 'https://chat.whatsapp.com/E8LJ85LRwfCFb92jev7Omn'
+global.web = 'https://chat.whatsapp.com/E8LJ85LRwfCFb92jev7Omn' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+
+//=======『 Tampilan Dan Lainnya 』============//
+global.fotonya1 = 'https://telegra.ph/file/ef6fbb13e88b671c58a25.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/ef6fbb13e88b671c58a25.jpg' //ini juga ganti 
+global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
+global.wm = 'Shinomiya-𝙼𝙳'
+global.watermark = wm
+global.wm2 = '                     「 ShinomiyaBot-𝙼𝙳 あ⁩ 」'
+global.wm3 = '⫹⫺ ShinomiyaBot-𝙼𝙳'
+global.htki = '––––––『' 
+global.htka = '』––––––'
+global.media = 'https://telegra.ph/file/fa36b2d4cd04bb2233f5b.jpg'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.wait = '_*tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.benar = 'Benar ✅\n'
+global.salah = 'Salah ❌\n'
+global.stiker_wait = 'Stiker sedang dibuat'
+global.packname = 'Bot nolep'
+global.author = '62895627514070'
+
+//=============『 Apikey 』================== //
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+    bx: 'https://bx-hunter.herokuapp.com',
+  dhnjing: 'https://dhnjing.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  erdwepe: 'https://erdwpe-api.herokuapp.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://melcanz.com': 'elaina',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://zekais-api.herokuapp.com': 'apikeymu',
+  'https://api.lolhuman.xyz': 'Deffbotz',
 }
 
-// Apikey Website Api
-global.APIKeys = {
-	'https://zenzapi.xyz': 'Your Key',
+//=============『 RPG GAMES 』================== //
+global.multiplier = 69 // The higher, The harder levelup
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
 }
 
-// Other
-global.owner = ['6281252848955']
-global.premium = ['0']
-global.ownername = 'Zetaa'
-global.botname = 'Zets Public'
-global.packname = '© ZETS - MULTI DEVICE'
-global.gc = 'https://bit.ly/3KMlmv2'
-global.limitawal = '100'
-global.author = '@naando.jpeg\n@alya.xzy'
-global.sessionName = 'sessionya'
-global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⌕'
-global.mess = {
-    success: 'Berhasil',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Tunggu sebentar, sedang di proses',
-    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
-}
-global.limitawal = {
-    premium: "Infinity",
-    free: 100
-}
-global.sc = fs.readFileSync('./media/sc.jpg')
-global.tq = fs.readFileSync('./media/tq.jpg')
-global.menu = fs.readFileSync('./media/menu.jpg')
-global.allmenu = fs.readFileSync('./media/allmenu.jpg')
-global.thumb = fs.readFileSync('./media/pepe.jpg')
-global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
-
+//===========『 Jangan Di Ubah 』================ //
+let fs = require('fs')
+let chalk = require('chalk')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
 })
